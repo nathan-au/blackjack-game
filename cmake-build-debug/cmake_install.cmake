2ace7b05cb7b1977f6eae06802c015dbde737850
nathan-au <im.nathanau@gmail.com>
2025-03-22 00:16:51 -0400

    build(cmake): update cmake build files

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/nathanau/Developer/CLion/blackjack game
+# Install script for directory: /Users/nathanau/Developer/CLion/blackjack
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "/Users/nathanau/Developer/CLion/blackjack game/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/Users/nathanau/Developer/CLion/blackjack/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
