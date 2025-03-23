a459ddcc327cbca820c149246d6f3d023fc5cabc
nathan-au <im.nathanau@gmail.com>
2025-03-23 01:59:31 -0400

    refactor(dealing): split int main() into different functions

--- blackjack.cpp
+++ blackjack.cpp
@@ -1,131 +1,100 @@
 #include <iostream>
-#include <stdlib.h>
-#include <time.h>
+#include <cstdlib>
+#include <ctime>
+#include <map>
 using namespace std;
 
-string* createDeck() {
-    const string suits[4] = {"Clubs", "Diamonds", "Hearts", "Spades"};
-    const string ranks[13] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
-    static string deck[52] = {};
+const string suits[4] = {"Clubs", "Diamonds", "Hearts", "Spades"};
+const string ranks[13] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+map<string, int> values = {{"A", 1}, {"2", 2}, {"3", 3}, {"4", 4}, {"5", 5}, {"6", 6}, {"7", 7}, {"8", 8}, {"9", 9}, {"10", 10}, {"J", 10}, {"Q", 10}, {"K", 10}};
 
-    int card_index = 0;
+vector <string> createDeck() {
+
+    vector <string> deck;
+
     for (const auto & suit : suits) {
         for (const auto & rank : ranks) {
-            deck[card_index] = rank + " of " + suit;
-            card_index++;
+            deck.push_back(rank + " of " + suit);
         }
     }
 
     srand(time(nullptr));
-    // int shuffle_swaps = rand() % 500;
     for (int x = 0; x < 1000; x++) {
         int card_1_index = rand() % 52;
         int card_2_index = rand() % 52;
-
         swap(deck[card_1_index], deck[card_2_index]);
     }
 
     return deck;
 }
 
+int calculateHandScore(const vector <string> & hand) {
+    int hand_score = 0;
+
+    for (const auto & card : hand) {
+        string rank = card.substr(0, card.find(' '));
+        hand_score +=  values.at(rank);
+    }
+
+    return hand_score;
+}
+
+int getPlayerChoice() {
+    int choice;
+
+    cout << "Hit (1) or Stand (2): ";
+    cin >> choice;
+    while (true) {
+        if (cin.fail() || (choice != 1 && choice != 2)) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid input. Try again: ";
+            cin >> choice;
+        }
+        else {
+            break;
+        }
+    }
+
+    return choice;
+}
+
 int player_balance = 0;
 int player_choice;
-string player_hand[11] = {};
-string dealer_hand[11] = {};
 
 int player_hand_score[2] = {0, 0};
 int dealer_hand_score = 0;
 
 int main() {
-    string *deck = createDeck();
+    const vector <string> deck = createDeck();
     int deck_index = 0;
 
-    // for (int x = 0; x < 52; x++) {
-    //     cout << deck[x] << endl;
+    // for (const auto & card : deck) {
+    //     cout << card << endl;
     // }
+    // cout << endl;
 
-    player_hand[0] = deck[0];
-    player_hand[1] = deck[2];
-    dealer_hand[0] = deck[1];
-    dealer_hand[1] = deck[3];
+    vector <string> player_hand = {deck[deck_index++], deck[deck_index++]};
+    vector <string> dealer_hand = {deck[deck_index++], deck[deck_index++]};
 
     cout << "Dealer's hand: " << endl;
-    for (int x = 0; x < 2; x++) {
-        cout << dealer_hand[x] << endl;
+    for (const auto & card : dealer_hand) {
+        cout << card << endl;
     }
+    cout << calculateHandScore(dealer_hand) << endl;
     cout << endl;
-    bool ace = false;
 
     cout << "Your hand: " << endl;
-    for (int x = 0; x < 2; x++) {
-        cout << player_hand[x] << endl;
-        // cout << player_hand[x][0] << endl;
-        int card_value = 0;
-        // cout << player_hand_score << endl;
-        if (isdigit(player_hand[x][0])) {
-            card_value = player_hand[x][0] - '0'; // convert ASCII char to type int
-
-            if (isdigit(player_hand[x][1])) {
-                card_value = 10;
-            }
-            player_hand_score[0] += card_value;
-            player_hand_score[1] += card_value;
-
-        }
-        else if (!isdigit(player_hand[x][0])) {
-
-            if (player_hand[x][0] == 'J' || player_hand[x][0] == 'Q' || player_hand[x][0] == 'K') {
-                card_value = 10;
-
-                player_hand_score[0] += card_value;
-                player_hand_score[1] += card_value;
-
-            }
-            else if (player_hand[x][0] == 'A') {
-                cerr << "ace" << endl;
-                ace = true;
-                player_hand_score[0] += 1;
-                player_hand_score[1] += 11;
-            }
-            else {
-                cout << "not digit" << endl;
-            }
-        }
+    for (const auto & card : player_hand) {
+        cout << card << endl;
     }
-
-    if (player_hand_score[0] != player_hand_score[1]) {
-        cout << "Hand score: " << player_hand_score[0] << " / " << player_hand_score[1] << endl;
-    }
-    else {
-        cout << "Hand score: " << player_hand_score[0] << endl;
-    }
+    cout << calculateHandScore(player_hand) << endl;
     cout << endl;
 
-    cout << "Hit (1) or Stand (2): ";
-    cin >> player_choice;
-
-    // VALIDATE player_choice (type int, 1, or 2)
-    while (true) {
-        if (cin.fail()) {
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            cout << "Invalid input. Try again: ";
-            cin >> player_choice;
-        }
-        else if (!cin.fail()) {
-            if (player_choice != 1 && player_choice != 2) {
-                cout << "Invalid input. Try again: ";
-                cin >> player_choice;
-            }
-            else if (player_choice == 1 || player_choice == 2) {
-                break;
-            }
-        }
-    }
+    player_choice = getPlayerChoice();
 
     if (player_choice == 1) {
         cout << "You hit" << endl;
-
     }
     else if (player_choice == 2) {
         cout << "You stand" << endl;
